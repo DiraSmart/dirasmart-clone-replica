43d27d61c8dcde30204e21c7875e5de22ba94871
DiraSmart <info@dirasmart.com>
2025-12-10 23:31:28 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import viteReact from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,7 +12,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     allowedHosts: ['new.dirasmart.com']
   },
-  plugins: [react(), cloudflare(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [viteRreact(), cloudflare(viteEnvironment: { name: "ssr" },), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
